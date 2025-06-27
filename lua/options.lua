d2265153205a3134044e394a0a5820a2cc7f4798
RandomHuman <193017945+RandomizedHuman@users.noreply.github.com>
2025-06-27 17:18:00 -0400

    Add files via upload

--- lua/options.lua
+++ lua/options.lua
@@ -1,1 +1,23 @@
+local opt = vim.opt
 
+opt.clipboard = 'unnamedplus'
+
+opt.mouse = 'a'
+
+opt.shiftwidth = 2
+opt.tabstop = 2
+opt.softtabstop = 2
+
+opt.number = true
+opt.relativenumber = true
+opt.cursorline = true
+
+opt.incsearch = true
+opt.hlsearch = false
+
+opt.splitbelow = true
+opt.splitright = true
+
+vim.o.guicursor = 'a:ver5'
+
+vim.cmd('colorscheme retrowave')
